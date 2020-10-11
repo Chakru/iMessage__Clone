1be2320d873bf8fb878519fd07d267ca6ed9bf5d
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-11 19:35:55 +0530

    #07 Firebase Auth login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase/firebase';
+import Login from './Components/Login/Login';
 import Imessage from './Components/iMessage/iMessage';
 
+import './App.css';
+
 function App() {
-  return (
-    <div className="app">
-      <Imessage />
-    </div>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
