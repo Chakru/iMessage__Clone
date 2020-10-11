605bb954e010f73f9144252133fc4e1538a1c23a
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-11 21:43:28 +0530

    #9 New chat creation, db realtime updates

--- src/Components/Sidebar/SidebarChats/SidebarChats.js
+++ src/Components/Sidebar/SidebarChats/SidebarChats.js
@@ -1,12 +1,12 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './SidebarChats.css';
-function SidebarChats() {
+function SidebarChats({ id, chatName }) {
   return (
     <div className="sidebarChat">
       <Avatar />
       <div className="sidebarChats__info">
-        <h3>Channel Name</h3>
+        <h3>{chatName}</h3>
         <p>Last Message</p>
         <small>Timestamp</small>
       </div>
