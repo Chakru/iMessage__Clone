731a205f1958d870713cc31d7e2bbae5e3adda62
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-11 15:14:35 +0530

    #03 iMessage Sidebar structure

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import RateReviewIcon from '@material-ui/icons/RateReview';
+import SidebarChats from './SidebarChats/SidebarChats';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar className="sidebar__avatar" />
+        <div className="sidebar__input">
+          <SearchIcon />
+          <input placeholder="Search" />
+        </div>
+        <IconButton variant="outlined" className="sidebar__inputButton">
+          <RateReviewIcon />
+        </IconButton>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChats />
+        <SidebarChats />
+        <SidebarChats />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
