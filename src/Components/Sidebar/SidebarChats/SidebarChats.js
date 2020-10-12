9fa440f00cabdbf5c92d8b77ef79bc862e84af97
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2020-10-13 00:32:47 +0530

    end to end functionality

--- src/Components/Sidebar/SidebarChats/SidebarChats.js
+++ src/Components/Sidebar/SidebarChats/SidebarChats.js
@@ -1,14 +1,44 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { setChat } from '../../../features/chatSlice';
+import { useDispatch } from 'react-redux';
+import db from '../../../firebase/firebase';
+import moment from 'moment';
 import './SidebarChats.css';
+
 function SidebarChats({ id, chatName }) {
+  const dispatch = useDispatch();
+  const [chatInfo, setChatInfo] = useState([]);
+
+  useEffect(() => {
+    db.collection('chats')
+      .doc(id)
+      .collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapshot =>
+        setChatInfo(snapshot.docs.map(doc => doc.data()))
+      );
+  }, [id]);
+
   return (
-    <div className="sidebarChat">
-      <Avatar />
+    <div
+      onClick={() =>
+        dispatch(
+          setChat({
+            chatId: id,
+            chatName: chatName,
+          })
+        )
+      }
+      className="sidebarChat"
+    >
+      <Avatar src={chatInfo[0]?.photo} />
       <div className="sidebarChats__info">
         <h3>{chatName}</h3>
-        <p>Last Message</p>
-        <small>Timestamp</small>
+        <p>{chatInfo[0]?.message}</p>
+        <small>
+          {moment(new Date(chatInfo[0]?.timestamp?.toDate())).fromNow()}
+        </small>
       </div>
     </div>
   );
